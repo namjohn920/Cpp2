b408936c22681a2fb15d3e14c08d08c06a3f2050
namjohn920 <40031678+namjohn920@users.noreply.github.com>
2018-06-07 10:24:45 -0400

    Add files via upload

---
+++ tools.h
@@ -0,0 +1,16 @@
+//John HyunKyu Nam
+//namjohn920@gmail.com
+
+#ifndef JOHN_TOOLS_H 
+#define JOHN_TOOLS_H
+
+#include <iostream>
+
+namespace sict {
+	// Displays the user interface menu
+	int menu();
+	// Performs a fool-proof integer entry
+	int getInt(int min, int max);
+}
+
+#endif
